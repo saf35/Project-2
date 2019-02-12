a5f0e1ace3c7ace7bc9b0269e60ac4652a5eacef
saf35 <saf35@duke.edu>
2019-02-12 12:58:39 -0500

    started working on letters class

--- sketch-practice.js
+++ sketch-practice.js
@@ -3,10 +3,13 @@ Have random letters pop up, and when someone sees a word, they type it in and th
 letter pops up again, it may be accompanied by that word
 */
 
-// Current step: 
+// Current step: have letters pop out from middle of screen and move outward at slow speed
 
 // load strings of lowercase letters
-let letters = ['a','b','c','d','e','f','g','h','i','j','k','j','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let letterslist = ['a','b','c','d','e','f','g','h','i','j','k','j','l','m','n','o','p','q','r','s','t','u','v','w',
+                'x','y','z'];
+// set up variable to hold letters currently on screen
+let currentLetters = [];
 
 function setup(){
     createCanvas(windowWidth,windowHeight);
@@ -15,4 +18,21 @@ function setup(){
 function draw(){
     background(0);
 
+}
+
+class Letters(){
+    appear(){ // letters appear
+        // Pick random letter from letters array
+        this.letter = lettersList[random(0,25)];
+        
+
+    }
+
+    move(){ // letters move outward at slow pace
+
+    }
+
+    display(){
+        // Start showing letter at center and then move them outward
+    }
 }
