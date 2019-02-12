45c92f1ce1f288b1d2cf21ff7cf037a9653fb52d
saf35 <saf35@duke.edu>
2019-02-12 12:50:18 -0500

    add array of letters

--- sketch-practice.js
+++ sketch-practice.js
@@ -1,7 +1,18 @@
-function setup(){
+/* 
+Have random letters pop up, and when someone sees a word, they type it in and then whenever that beginning
+letter pops up again, it may be accompanied by that word
+*/
 
+// Current step: 
+
+// load strings of lowercase letters
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','j','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
 }
 
 function draw(){
-    
+    background(0);
+
 }
