dd556b3431e31f2b137f3e696f8bfa5884e6639c
saf35 <saf35@duke.edu>
2019-02-13 16:20:37 -0500

    add to Letters func, update func draw

--- sketch-practice.js
+++ sketch-practice.js
@@ -3,13 +3,18 @@ Have random letters pop up, and when someone sees a word, they type it in and th
 letter pops up again, it may be accompanied by that word
 */
 
-// Current step: have letters pop out from middle of screen and move outward at slow speed
+// Current step: have letters pop drop from top of screen and move downward
 
 // load strings of lowercase letters
-let letterslist = ['a','b','c','d','e','f','g','h','i','j','k','j','l','m','n','o','p','q','r','s','t','u','v','w',
-                'x','y','z'];
+let letterslist = ['a','b','c','d','e','f','g','h','i','j','k','j','l','m','n','o','p','q','r','s','t','u','v',
+                    'w','x','y','z'];
 // set up variable to hold letters currently on screen
 let currentLetters = [];
+
+let reaBot = false;
+
+let xposArray = [];
+let yposArray = [];
 
 function setup(){
     createCanvas(windowWidth,windowHeight);
@@ -17,22 +22,37 @@ function setup(){
 
 function draw(){
     background(0);
-
+    // load function into variable
+    let l = new Letters();
+    currentLetters.push(l);
+    // display moving letters
+    for(let i=0; i<currentLetters.length; i++){
+        currentLetters.display();
+        currentLetters.move();
+    }
 }
 
 class Letters(){
-    appear(){ // letters appear
+    appear(){ // letters appear (aka, choose a new letter)
         // Pick random letter from letters array
         this.letter = lettersList[random(0,25)];
+        // store it in current letters array
+        currentLetters[i] = this.letter;
         
-
     }
 
-    move(){ // letters move outward at slow pace
-
+    move(){ // letters move down the screen at slow pace
+        xspeed = 0.1;
+        yspeed = random();
+        this.x += xspeed;
+        this.y += yspeed;
+        if(this.y > windowHeight){
+            this.y = 0;
+        }
     }
 
     display(){
-        // Start showing letter at center and then move them outward
+        text(this.letter,this.x,this.y);
+        fill(0);
     }
 }
