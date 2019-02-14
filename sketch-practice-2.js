621c0f13b1788081fbb29e3d66ed0c603e0452a7
saf35 <saf35@duke.edu>
2019-02-13 20:03:48 -0500

    palettes

--- sketch-practice-2.js
+++ sketch-practice-2.js
@@ -1,3 +1,22 @@
 /*
 Click on different buttons, algorithm produces season-inspired patterns
+
+Spring color palette: mostly green, with splashes of the rainbow for flowers and blue for the 
+sky
+
+Summer color palette: Blue for sky, sand color for ground
+
+Fall color palette: Dark green/brown for tree trunks, and orange-red & red for leaves and ground,
+maybe with some green
+
+Winter color palette: Very light or very deep blue-purple, with browns and greys, and 
+yellow-orange for the Sun
 */
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+    background(0);
+}
