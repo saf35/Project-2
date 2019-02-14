e41d6ddf013cce90dfef270f852d8d0fed77a274
saf35 <saf35@duke.edu>
2019-02-13 20:10:08 -0500

    looking at particle code from jp5s examples

--- sketch-practice-2.js
+++ sketch-practice-2.js
@@ -15,10 +15,72 @@ yellow-orange for the Sun
 
 // Current goal: build spring graphic
 
+var system; // From https://p5js.org/examples/simulate-particle-system.html
+
 function setup(){
     createCanvas(windowWidth,windowHeight);
+
+    // From https://p5js.org/examples/simulate-particle-system.html
+    system = new ParticleSystem(createVector(width/2, 50)); 
 }
 
 function draw(){
     background(0);
+
+    // From https://p5js.org/examples/simulate-particle-system.html
+    system.addParticle();
+    system.run();
 }
+
+// particle function
+var Particle = function(position) { // From https://p5js.org/examples/simulate-particle-system.html
+    this.acceleration = createVector(0, 0.05); 
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 400;
+  };
+
+  Particle.prototype.run = function() { // From https://p5js.org/examples/simulate-particle-system.html
+    this.update();
+    this.display();
+  };
+
+  // Method to update position
+  Particle.prototype.update = function(){ // From https://p5js.org/examples/simulate-particle-system.html
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  };
+
+  // Method to display
+  Particle.prototype.display = function() { // From https://p5js.org/examples/simulate-particle-system.html
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(127, this.lifespan);
+    ellipse(this.position.x, this.position.y, 12, 12);
+  };
+
+  // Is the particle still useful?
+  Particle.prototype.isDead = function(){ // From https://p5js.org/examples/simulate-particle-system.html
+    return this.lifespan < 0;
+  };
+  
+  var ParticleSystem = function(position) { // From https://p5js.org/examples/simulate-particle-system.html
+    this.origin = position.copy();
+    this.particles = [];
+  };
+  
+  // From https://p5js.org/examples/simulate-particle-system.html
+  ParticleSystem.prototype.addParticle = function() { 
+    this.particles.push(new Particle(this.origin));
+  };
+  
+  ParticleSystem.prototype.run = function() { // From https://p5js.org/examples/simulate-particle-system.html
+    for (var i = this.particles.length-1; i >= 0; i--) {
+      var p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  };
